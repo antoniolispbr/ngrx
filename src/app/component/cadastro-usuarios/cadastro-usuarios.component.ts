17df293338dad8877d6bc4da3a5499cee32695ec
Rogerio <antoniolispbr@gmail.com>
2024-05-15 09:20:42 -0300

    criacao do cadastro-usuarios

--- src/app/component/cadastro-usuarios/cadastro-usuarios.component.ts
+++ src/app/component/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { UsuarioSevice } from '../../service/usuario.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro-usuarios',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './cadastro-usuarios.component.html',
   styleUrl: './cadastro-usuarios.component.css'
 })
 export class CadastroUsuariosComponent {
+  model: UsuarioModel = { id: 0, nome: '', idade: 0, perfil: ''};
+
+  constructor(private usuarioService: UsuarioSevice){}
+
+  addUsuario(){
+    if(this.model.id == 0){
+      //cadastra
+      this.usuarioService.addUsuario(this.model).subscribe();
+    }else {
+      //atualizar
+    }
+  }
 
 }
--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ListarUsuariosComponent } from '../listar-usuarios/listar-usuarios.component';
+import { CadastroUsuariosComponent } from '../cadastro-usuarios/cadastro-usuarios.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ListarUsuariosComponent],
+  imports: [ListarUsuariosComponent, CadastroUsuariosComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
